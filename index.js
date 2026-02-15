9f2c7314359c69ff99260eda920ab2d344badfbd
ebarlock20 <ebarlock20@gmail.com>
2026-02-15 16:07:56 -0500

    Remove HTML tags from poem generation message

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function generatePoem(event) {
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="blink">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `⏳ Generating a French poem about ${instructionsInput.value}`;
 
   console.log("Generating poem...");
   console.log(`Prompt: ${prompt}`);
